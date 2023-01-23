4e9773fa46ee58569dce1b30a13d5cc3557b6274
Filip Jelic <fjelic@tripadvisor.com>
2023-01-23 14:04:29 +0100

    lambda example

---
+++ lambda_example/lambda_handler.py
@@ -0,0 +1,15 @@
+import sys
+
+from ghostbuster.cli import cli
+
+
+def lambda_handler(event, context):
+    # sys.argv = ['ghostbuster', 'scan', 'aws', '--roles', 'roles.csv', '--regions', 'us-east-1' '--skipascii']
+
+    org_lookup_role_arn = 'arn:aws:iam::474234877840:role/ta-application-security-prd-ghostbuster-org-role'
+    sys.argv = ['ghostbuster', 'scan', 'aws', '--autoroles', org_lookup_role_arn, '--regions', 'us-east-1' '--skipascii']
+    cli()
+    return {
+        'statusCode': 200,
+        'body': f'Gracefully finished running {" ".join(sys.argv)}'
+    }
