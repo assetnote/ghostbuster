e71257807954de60babcaa8f987c55097998b8b3
Filip Jelic <fjelic@tripadvisor.com>
2023-01-23 14:16:20 +0100

    improve lambda example

--- lambda_example/lambda_handler.py
+++ lambda_example/lambda_handler.py
@@ -5,8 +5,8 @@ from ghostbuster.cli import cli
 
 def lambda_handler(event, context):
     # sys.argv = ['ghostbuster', 'scan', 'aws', '--roles', 'roles.csv', '--regions', 'us-east-1' '--skipascii']
-
-    org_lookup_role_arn = 'arn:aws:iam::474234877840:role/ta-application-security-prd-ghostbuster-org-role'
+    org_lookup_account_id = '111222333444'  # TODO change me!
+    org_lookup_role_arn = f'arn:aws:iam::{org_lookup_account_id}:role/ta-application-security-prd-ghostbuster-org-role'
     sys.argv = ['ghostbuster', 'scan', 'aws', '--autoroles', org_lookup_role_arn, '--regions', 'us-east-1' '--skipascii']
     cli()
     return {
