b3f0e84ab87fb64f2eb09c8aab1cafc3a088c20b
Filip Jelic <fjelic@tripadvisor.com>
2023-01-30 17:37:07 +0100

    test all relevant flags for MR

--- lambda_example/lambda_handler.py
+++ lambda_example/lambda_handler.py
@@ -4,12 +4,29 @@ from ghostbuster.cli import cli
 
 
 def lambda_handler(event, context):
-    # sys.argv = ['ghostbuster', 'scan', 'aws', '--roles', 'roles.csv', '--regions', 'us-east-1' '--skipascii']
+    # --roles roles.csv
+    sys.argv = ['ghostbuster', 'scan', 'aws', '--roles', 'roles.csv', '--regions', 'us-east-1', '--skipascii']
+    try:
+        cli()
+    except Exception as e:
+        print(str(e))  # some weird Runtime.Exit error 
 
+    # --json --roles roles.csv
+    sys.argv = ['ghostbuster', 'scan', 'aws', '--roles', 'roles.csv', '--regions', 'us-east-1', '--skipascii', '--json']
+    try:
+        cli()
+    except Exception as e:
+        print(str(e))
+
+    # --autoroles org_lookup_role_arn
     org_lookup_role_arn = 'arn:aws:iam::474234877840:role/ta-application-security-prd-ghostbuster-org-role'
     sys.argv = ['ghostbuster', 'scan', 'aws', '--autoroles', org_lookup_role_arn, '--regions', 'us-east-1', '--skipascii']
-    cli()
+    try:
+        cli()
+    except Exception as e:
+        print(str(e))
+
     return {
         'statusCode': 200,
-        'body': f'Gracefully finished running {" ".join(sys.argv)}'
+        'body': f'Finished running {" ".join(sys.argv)}'
     }
