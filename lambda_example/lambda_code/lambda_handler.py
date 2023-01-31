a2ae1ee0ca4ce956c26e34934fc28aa21b4bdb48
Filip Jelic <fjelic@tripadvisor.com>
2023-01-31 13:38:43 +0100

    remove try except as it doesnt work

--- lambda_example/lambda_code/lambda_handler.py
+++ lambda_example/lambda_code/lambda_handler.py
@@ -5,27 +5,25 @@ from ghostbuster.cli import cli
 
 def lambda_handler(event, context):
     # --roles roles.csv
-    sys.argv = ['ghostbuster', 'scan', 'aws', '--roles', 'roles.csv', '--regions', 'us-east-1', '--skipascii']
-    try:
-        cli()
-    except Exception as e:
-        print(str(e))  # some weird Runtime.Exit error 
+    # sys.argv = ['ghostbuster', 'scan', 'aws', '--roles', 'roles.csv', '--regions', 'us-east-1', '--skipascii']
+    # cli()
 
     # --json --roles roles.csv
-    sys.argv = ['ghostbuster', 'scan', 'aws', '--roles', 'roles.csv', '--regions', 'us-east-1', '--skipascii', '--json']
-    try:
-        cli()
-    except Exception as e:
-        print(str(e))
+    # sys.argv = ['ghostbuster', 'scan', 'aws', '--roles', 'roles.csv', '--regions', 'us-east-1', '--skipascii', '--json']
+    # cli()
 
     # --autoroles org_lookup_role_arn
     org_lookup_role_arn = 'arn:aws:iam::474234877840:role/ta-application-security-prd-ghostbuster-org-role'
     sys.argv = ['ghostbuster', 'scan', 'aws', '--autoroles', org_lookup_role_arn, '--regions', 'us-east-1', '--skipascii']
-    try:
-        cli()
-    except Exception as e:
-        print(str(e))
+    cli()
+    """ 
+        cli completes the task, but then ends with
+    
+    Error: Runtime exited without providing a reason 
+    Runtime.ExitError
 
+    This results in lambda ending with Internal Server Error instead of returning 200 below:
+    """
     return {
         'statusCode': 200,
         'body': f'Finished running {" ".join(sys.argv)}'
